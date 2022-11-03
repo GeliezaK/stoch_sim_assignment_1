d9fd2675620ce615d422ba868bbf3947d9b6dec9
JariMHoffmann <jari.hoffmann@gmail.com>
2022-11-03 16:33:20 +0100

    added is_stable test

--- calculate_area.py
+++ calculate_area.py
@@ -29,21 +29,33 @@ def make_random_point():
     c = complex((r * math.cos(alpha) + circle_x), (r * math.sin(alpha) + circle_y ))
     return c
 
+def is_stable(c, num_iterations):
+    # Check if the given complex number is part of the mandelbrot set
+    z = 0
+    for _ in range(num_iterations):
+        z = z ** 2 + c
+        if abs(z) > 2:
+            return False
+    return True
+
 def draw_random_numbers(s):
-    samples = np.zeros(s, dtype=np.csingle)
-    for i in range(s):
+    # samples = np.zeros(s, dtype=np.csingle)
+    samples = []
+    for _ in range(s):
         c = make_random_point()
-        samples[i] = c
+        if is_stable(c, 200):
+            samples.append(c)
+    samples = np.array(samples)
     return samples
 
 if __name__ == '__main__':
     width = 400
     height = 400
-    im = Image.new("RGB", (400, 400), color ="black")
+    im = Image.new("RGB", (400, 400), color ="white")
     draw = ImageDraw.Draw(im)
-    samples = draw_random_numbers(50000)
+    samples = draw_random_numbers(500000)
     for i in range(len(samples)):
         real = width/2 + np.real(samples[i]) * width/4
         imag = height/2 + np.imag(samples[i]) * height/4  # should be height/(2*radius)
-        draw.point((real, imag), fill="white")
+        draw.point((real, imag), fill="black")
     im.show()
